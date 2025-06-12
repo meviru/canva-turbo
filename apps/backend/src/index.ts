c1b70ae1e2d6205e0333da974bf8248a47c1a348
Viral Patel <viralpatel508@gmail.com>
2025-06-12 18:21:29 +0530

    feat: setup backend in a monorepo

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running at http://localhost:${PORT}`);
+});
