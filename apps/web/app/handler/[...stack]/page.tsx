63a1bd96ee1a4f28b06f42b303bf9ce833b3d0d5
Viral Patel <viralpatel508@gmail.com>
2025-06-16 18:58:30 +0530

    feat: design ui for auth pages

--- apps/web/app/handler/[...stack]/page.tsx
+++ apps/web/app/handler/[...stack]/page.tsx
@@ -1,6 +1,19 @@
 import { StackHandler } from "@stackframe/stack";
 import { stackServerApp } from "../../../stack";
+import logo from "public/Canva_Logo.svg";
+import authBg from "public/authentication-bg.webp";
+import Image from "next/image";
 
 export default function Handler(props: unknown) {
-  return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
+  return (
+    <div className="h-screen grid place-items-center w-full relative z-10 bg-cover bg-no-repeat" style={{ backgroundImage: `url(${authBg.src})` }}>
+      <Image src={logo.src} width={80} height={30} alt="Canva" className="absolute top-6 left-6 brightness-0 invert-100" />
+      <div className="absolute inset-0 bg-black opacity-50 -z-10"></div>
+      <div className="absolute top-0 left-0 right-0 h-[250px] bg-linear-to-b from-black/90 to-transparent -z-10"></div>
+      <div className="absolute bottom-0 left-0 right-0 h-[250px] bg-linear-to-t from-black/90 to-transparent -z-10"></div>
+      <div className="bg-white p-6 pb-7 w-[400px] mx-auto flex items-center justify-center rounded-lg">
+        <StackHandler fullPage={false} app={stackServerApp} routeProps={props} />
+      </div>
+    </div>
+  )
 }
